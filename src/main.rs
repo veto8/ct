b708eb1a64b547c7e5540cee180eb3cc52fb7f99
veto <veto@lab.local>
2026-04-09 07:35:49 +0700

    up: src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
+use egui::Vec2;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        //initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        viewport: egui::ViewportBuilder::default().with_inner_size(Vec2::new(900.0, 700.0)), // Set initial window size here
         ..Default::default()
     };
     eframe::run_native(
@@ -13,7 +14,7 @@ fn main() -> Result<(), eframe::Error> {
         Box::new(|cc| {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
-
+            cc.egui_ctx.set_pixels_per_point(2.5);
             Box::new(CT::default())
         }),
     )
@@ -23,7 +24,6 @@ fn main() -> Result<(), eframe::Error> {
 struct CT {
     label: String,
     counter: usize,
-
     text: String,
     picked_path: String,
     cursor1: usize,
@@ -37,7 +37,43 @@ impl eframe::App for CT {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add_space(20.0);
+            ui.horizontal(|ui| {
+                let num_buttons = 6.0;
+                let spacing = ui.spacing().item_spacing.x;
+                let total_spacing = spacing * (num_buttons - 1.0);
 
+                let available_width = ui.available_width();
+                let button_width = (available_width - total_spacing) / num_buttons;
+                let button_height = 20.0;
+                let button_size = egui::Vec2::new(button_width, button_height);
+
+                if ui
+                    .add(egui::Button::new("Open").min_size(button_size))
+                    .clicked()
+                {}
+
+                if ui
+                    .add(egui::Button::new("Save").min_size(button_size))
+                    .clicked()
+                {}
+
+                if ui
+                    .add(egui::Button::new("Cut").min_size(button_size))
+                    .clicked()
+                {}
+                if ui
+                    .add(egui::Button::new("Copy").min_size(button_size))
+                    .clicked()
+                {}
+                if ui
+                    .add(egui::Button::new("Paste").min_size(button_size))
+                    .clicked()
+                {}
+                if ui
+                    .add(egui::Button::new("Close").min_size(button_size))
+                    .clicked()
+                {}
+            });
             let _password = ui.add(
                 egui::TextEdit::singleline(&mut self.password)
                     .hint_text("Please enter your password")
